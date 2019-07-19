9f6dbbd0ff0b57b462a4ea90f9ce83cb8e917764
Maxeem <jmax.posh@gmail.com>
2019-07-19 23:17:23 +0300

    Add first Navigation

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,26 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener {
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+//            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+        return binding.root
+    }
+
+}
