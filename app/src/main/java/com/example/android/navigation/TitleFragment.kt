6d76ca3684ac0abd5aac5320fed4e31f2f20d24a
Maxeem <jmax.posh@gmail.com>
2019-07-20 14:13:31 +0300

    Complete Exercise: Adding a Menu

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,13 +2,12 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -17,12 +16,17 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        setHasOptionsMenu(true)
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.app_name)
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return item?.let { NavigationUI.onNavDestinationSelected(it, findNavController()) }
+                ?: super.onOptionsItemSelected(item)
     }
 
 }
